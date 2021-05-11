2584ed5f81c6cd9d2dc1bac425829000ba513395
felix <fsafaridis@hotmail.com>
2021-05-11 12:48:15 +0300

    common crudservice

--- pet-clinic-data/src/main/java/dev/rxvchicken/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/dev/rxvchicken/petclinic/services/PetService.java
@@ -5,9 +5,6 @@ import dev.rxvchicken.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long> {
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
